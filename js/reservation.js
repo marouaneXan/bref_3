2649a9ade1decaf02187cbe83998b4ae290271a6
unknown <marouanezahaoui835@gmail.com>
2022-01-04 15:13:35 +0100

    new files added

---
+++ js/reservation.js
@@ -0,0 +1,111 @@
+var type,prix=0,bonus_boite_vitesse,bonus_carburant;
+let a=document.getElementById("type_vehicule");
+a.onchange= function(type)
+{
+    type=a.value;
+    switch(type)
+    {
+        case "moto":
+            document.getElementById("hybride").disabled=true;
+            document.getElementById("essence").disabled=false;
+            document.getElementById("electrique").disabled=false;
+            document.getElementById("diesel").disabled=true;
+
+            document.getElementById("manuelle").disabled=true;
+            document.getElementById("automatique").disabled=true;
+            document.getElementById("autre").disabled=false;
+            break;
+        case "compact":
+            document.getElementById("hybride").disabled=false;
+            document.getElementById("essence").disabled=false;
+            document.getElementById("electrique").disabled=true;
+            document.getElementById("diesel").disabled=false;
+            document.getElementById("manuelle").disabled=false;
+            document.getElementById("automatique").disabled=true;
+            break;
+        case "citan":
+            document.getElementById("hybride").disabled=false;
+            document.getElementById("essence").disabled=false;
+            document.getElementById("electrique").disabled=false;
+            document.getElementById("diesel").disabled=false;
+            document.getElementById("manuelle").disabled=false;
+            document.getElementById("automatique").disabled=true;
+            prix=12;
+            bonus_carburant=44.05;
+            break;
+        case "utilitaire":
+            document.getElementById("hybride").disabled=true;
+            document.getElementById("essence").disabled=true;
+            document.getElementById("electrique").disabled=true;
+            document.getElementById("diesel").disabled=false;
+            document.getElementById("manuelle").disabled=false;
+            document.getElementById("automatique").disabled=true;
+            break;
+        case "berlin":
+            document.getElementById("hybride").disabled=false;
+            document.getElementById("essence").disabled=false;
+            document.getElementById("electrique").disabled=true;
+            document.getElementById("diesel").disabled=false;
+            document.getElementById("manuelle").disabled=true;
+            document.getElementById("automatique").disabled=false;
+            break;   
+        case "camion":
+            document.getElementById("hybride").disabled=true;
+            document.getElementById("essence").disabled=true;
+            document.getElementById("electrique").disabled=true;
+            document.getElementById("diesel").disabled=false;
+            document.getElementById("manuelle").disabled=true;
+            document.getElementById("automatique").disabled=false;
+            break; 
+    }
+} 
+
+let button=document.getElementById("submit");
+button.onclick=function() {
+    var car = document.getElementById('type_vehicule').value;
+    var carb=document.getElementById('carburant').value;
+    var boite = document.getElementById('boite-vitesse').value;
+    var duree_reservetion = document.getElementById('duree-reservation').value;
+    switch(car){
+        case "moto":
+            prix+=10;
+            break;
+            case "compact":
+                prix+=14;
+                break;
+            case "citan":
+                prix+=12;
+                break;
+            case "utilitaire":
+                prix+=16;
+                break;
+            case "berlin":
+                prix+=20;
+                break;   
+            case "camion":
+                prix+=250;
+                break; 
+    }
+    switch(carb){
+        case "essence":
+            bonus_carburant=0.14;
+            break;
+        case "diesel":
+            bonus_carburant=0.21;
+            break;
+        case "electrique":
+            bonus_carburant=0.5;
+            break;
+        case "hybride":
+            bonus_carburant=0.9;
+            break;
+    }
+    if(boite=="automatique"){
+        bonus_boite_vitesse=0.19;
+    }
+    else if(boite=="manuelle" || boite=="autre")
+        bonus_boite_vitesse=0;
+
+    var res=(prix+prix*(bonus_boite_vitesse+bonus_carburant))*duree_reservetion;
+    alert("Prix = "+res+" euro");
+}
